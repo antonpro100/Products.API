f08afbb7f89ebefd6382efd21031b05721a69d9a
Anton Dolivets <anton.dolivets@gmail.com>
2021-12-01 18:39:33 +0200

    Add project files.

---
+++ Core/Products.Domain/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Products.Domain
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public bool IsActive { get; set; }
+        public DateTime CteatedAt { get; set; }
+        public Guid CreatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public Guid? UpdatedBy { get; set; }
+    }
+}
