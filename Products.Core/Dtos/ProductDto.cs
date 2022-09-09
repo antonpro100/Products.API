4d737abeb14f10f4ba6f5c40ffc2da283fdcc333
Anton Dolivets <anton.dolivets@devspiration.com>
2022-09-09 14:57:32 +0300

    Create fix

--- Products.Core/Dtos/ProductDto.cs
+++ Products.Core/Dtos/ProductDto.cs
@@ -5,7 +5,7 @@ namespace Products.Core.Dtos
 {
     public class ProductDto
     {
-        public Guid Id { get; set; }
+        public Guid? Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string ImagePath { get; set; }
