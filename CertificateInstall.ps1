0b47e562b09a677ae19498efabb90f0bdd831bf9
Anton Dolivets <anton.dolivets@devspiration.com>
2022-09-22 12:59:30 +0300

    Docker compose

--- CertificateInstall.ps1
+++ CertificateInstall.ps1
@@ -1,4 +1,4 @@
-dotnet dev-certs https -ep $env:USERPROFILE\.aspnet\https\ProductsAPI.pfx -p 100Rage69
+dotnet dev-certs https -ep $env:USERPROFILE\.aspnet\https\Products.API.pfx -p 100Rage69
 dotnet dev-certs https --trust
 cd Products.API
 dotnet user-secrets set "Kestrel:Certificates:Development:Password" "100Rage69"
